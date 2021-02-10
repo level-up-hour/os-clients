a58088be166fea3ab82b4482960d90845002136f
langdon <langdon@fedoraproject.org>
2021-02-10 13:49:27 -0500

    relying on the fact that the oc.tar has kubectl inside it, we set up kubectl in launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -7,6 +7,10 @@ case $1 in
     /opt/openshift/oc "${@:2}"
     ;;
 
+  kubectl)
+    /opt/openshift/kubectl "${@:2}"
+    ;;
+
   helm)
     /opt/helm/helm-linux-amd64 "${@:2}"
     ;;
@@ -23,7 +27,7 @@ case $1 in
     if [ $1 != "help" ]; then
       echo "\"$1\" is not a supported command."
     fi
-    echo "Available commands are: oc, helm, template2helm, odo"
+    echo "Available commands are: oc, helm, template2helm, odo, kubectl"
     ;;
 
 esac
